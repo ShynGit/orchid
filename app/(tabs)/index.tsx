12cea7c7fb618ea746cc4738d1d615abc1c0430a
Shyn <huusy832gl@gmail.com>
2023-11-04 16:14:39 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,87 @@
+import { Tab, TabView, Text, makeStyles } from "@rneui/themed";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Dimensions, FlatList, View } from "react-native";
+import { useAppContext } from "../../context/Context";
+
+import PlantItem from "../../components/PlantItem";
+import { ScrollView } from "react-native-gesture-handler";
+import { useState } from "react";
+import { getAllCategories, getAllFlowerFromDb } from "../../utils";
+import { Categories } from "../../db";
+
+export default function TabOneScreen() {
+  const styles = useStyles();
+  const plants = Categories;
+  const [index, setIndex] = useState(0);
+  const cate = getAllCategories();
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          height: "100%",
+          marginTop: 70,
+        }}
+      >
+        <Tab
+          value={index}
+          onChange={(e) => setIndex(e)}
+          indicatorStyle={{
+            backgroundColor: "white",
+            height: 3,
+          }}
+          variant="primary"
+        >
+          {cate.map((item) => (
+            <Tab.Item
+              title={item}
+              titleStyle={{ fontSize: 12, fontWeight: "700" }}
+            />
+          ))}
+        </Tab>
+        <TabView value={index} onChange={setIndex} animationType="spring">
+          {cate.map((cate, index) => (
+            <TabView.Item
+              style={{
+                width: "100%",
+              }}
+            >
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{ marginBottom: 70 }}
+              >
+                <FlatList
+                  style={styles.list}
+                  showsVerticalScrollIndicator={false}
+                  scrollEnabled={false}
+                  data={plants[index].items}
+                  keyExtractor={(plant) => plant.name}
+                  renderItem={({ item }) => (
+                    <PlantItem item={item} type="normal" />
+                  )}
+                />
+              </ScrollView>
+            </TabView.Item>
+          ))}
+        </TabView>
+      </View>
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+    </View>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  list: {
+    width: "100%",
+    paddingBottom: 20,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    backgroundColor: theme.colors.white,
+  },
+  title: {
+    color: theme.colors.primary,
+    fontSize: 700,
+  },
+}));
---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@rneui/themed";
+
+export const lightColors = {
+  primary: "#00BB41",
+  secondary: "#FEFDF7",
+};
+
+export const theme = createTheme({
+  lightColors,
+});
